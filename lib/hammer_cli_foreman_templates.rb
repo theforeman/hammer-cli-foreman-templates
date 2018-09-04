fa22ddbe6681f4f52bfd8f27c523d4f2bcb2e992
Oleh Fedorenko <ofedoren@redhat.com>
2018-09-04 15:26:20 +0000

    Fixes #24627 - Unclear hammer error message while sync

--- lib/hammer_cli_foreman_templates.rb
+++ lib/hammer_cli_foreman_templates.rb
@@ -3,8 +3,13 @@ require 'hammer_cli_foreman/template'
 
 module HammerCLIForemanTemplates
   require 'hammer_cli_foreman_templates/version'
+  require 'hammer_cli_foreman_templates/exception_handler'
   require 'hammer_cli_foreman_templates/import'
   require 'hammer_cli_foreman_templates/export'
+
+  def self.exception_handler_class
+    HammerCLIForemanTemplates::ExceptionHandler
+  end
 
   HammerCLI::MainCommand.lazy_subcommand(
     ImportCommand.command_name,
---
+++ lib/hammer_cli_foreman_templates/exception_handler.rb
@@ -0,0 +1,23 @@
+module HammerCLIForemanTemplates
+  class ExceptionHandler < HammerCLIForeman::ExceptionHandler
+    def mappings
+      [
+        [RestClient::InternalServerError, :handle_internal_error]
+      ] + super
+    end
+
+    protected
+
+    def handle_internal_error(e)
+      handle_templates_error(e)
+      HammerCLI::EX_SOFTWARE
+    end
+
+    def handle_templates_error(e)
+      response = JSON.parse(e.response)
+      response = HammerCLIForeman.record_to_common_format(response)
+      print_error(response['error'] || response['warning'])
+      log_full_error e
+    end
+  end
+end
